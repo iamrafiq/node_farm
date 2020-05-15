55eff3b70d214ae51b98bb6de215e37448becc59
linuxrafiq <rafiq.i@live.co.uk>
2020-05-15 17:15:50 +0600

    now user able to see the detail of a product by clicking on detail button from overview page

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const replaceTemplate = require('./moduels/replaceTemplate');
 //loading data
 const templateOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
 const templateCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
@@ -9,23 +10,6 @@ const templateProduct = fs.readFileSync(`${__dirname}/templates/template-product
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
 const dataObj = JSON.parse(data);
 
-const replaceTemplate = (template, product)=>{
-    // using regular expression (/{%PRODUCTNAME%}/g) insted of "{%PRODUCTNAME%}"
-    //https://stackoverflow.com/questions/6052616/what-does-the-regular-expression-g-mean
-    let output = template.replace(/{%PRODUCTNAME%}/g, product.productName); 
-    output = output.replace(/{%IMAGE%}/g, product.image); 
-    output = output.replace(/{%PRICE%}/g, product.price); 
-    output = output.replace(/{%FROM%}/g, product.from); 
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients); 
-    output = output.replace(/{%QUANTITY%}/g, product.quantity); 
-    output = output.replace(/{%DESCRIPTION%}/g, product.description); 
-    output = output.replace(/{%ID%}/g, product.id); 
-    if(!product.organic){
-        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic'); 
-    }
-
-    return output;
-}
 const server = http.createServer((req, res)=>{
     //const pathname = req.url;
     const {query, pathname}=url.parse(req.url, true);
