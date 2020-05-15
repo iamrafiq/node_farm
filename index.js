eaf7c99245c410be96e697a3c11a9bfbb9e8e228
linuxrafiq <rafiq.i@live.co.uk>
2020-05-15 17:09:25 +0600

    now user able to see the detail of a product by clicking on detail button from overview page

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const replaceTemplate = (template, product)=>{
     output = output.replace(/{%FROM%}/g, product.from); 
     output = output.replace(/{%NUTRIENTS%}/g, product.nutrients); 
     output = output.replace(/{%QUANTITY%}/g, product.quantity); 
-    //output = output.replace(/{%DESCRIPTION%}/g, product.description); 
+    output = output.replace(/{%DESCRIPTION%}/g, product.description); 
     output = output.replace(/{%ID%}/g, product.id); 
     if(!product.organic){
         output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic'); 
@@ -27,8 +27,8 @@ const replaceTemplate = (template, product)=>{
     return output;
 }
 const server = http.createServer((req, res)=>{
-    const pathname = req.url;
-    const {query, pathname}=curl.parse(req.url, true);
+    //const pathname = req.url;
+    const {query, pathname}=url.parse(req.url, true);
     if(pathname === '/' || pathname === '/overview'){
         /**
          * Loading Template Overview, Content of the 
@@ -42,10 +42,12 @@ const server = http.createServer((req, res)=>{
         const output = templateOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
         res.end(output);
     }else if(pathname==='/product'){
+        const product = dataObj[query.id];
         res.writeHead(200,{
             'Content-type':'text/html'
         });
-        res.end('this is the product');
+        const output = replaceTemplate(templateProduct, product);
+        res.end(output);
     }else if(pathname === '/api'){
         //Non-blocking or Async way
        /* fs.readFile(`${__dirname}/dev-data/data.json`,'utf-8', (err, data)=>{
